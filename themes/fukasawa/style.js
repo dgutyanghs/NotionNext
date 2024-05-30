66a02f98c4e6a1727636c0e3297f985fb962701a
yyds <yyds@gmail.com>
2024-05-30 19:03:49 +0800

    fix grid.item height is 375px

--- themes/fukasawa/style.js
+++ themes/fukasawa/style.js
@@ -16,7 +16,8 @@ const Style = () => {
     
     /* fukasawa的首页响应式分栏 */
     #theme-fukasawa .grid-item {
-        height: auto;
+        // height: auto;
+        height: 375px;
         break-inside: avoid-column;
         margin-bottom: .5rem;
     }
